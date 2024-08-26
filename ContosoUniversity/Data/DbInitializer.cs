d65a6a6044145591e5436d92938995264edfcae0
Opilane TTHK <opilane@tthk.ee>
2024-08-26 17:35:02 +0300

    added dependency detection , db context , started adding initialitor

---
+++ ContosoUniversity/Data/DbInitializer.cs
@@ -0,0 +1,21 @@
+﻿using ContosoUniversity.Models;
+
+namespace ContosoUniversity.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(SchoolContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Students.Any())
+            {
+                return;
+            }
+            var students = new Student[]
+            {
+                new Student{FirstMidName="Artjom",LastName="SkatŠkov",EnrollmentDate=DateTime.Parse("2069-06-09")}
+            };
+        }
+    }
+}
--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -1,6 +1,15 @@
-﻿namespace ContosoUniversity.Models
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models
 {
     public class Student
     {
+        [Key] 
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        public ICollection<Enrollment> Enrollments { get; set; }
     }
 }
